b0b96d5e001ebf2ca0a53946648e062dc0982caf
Batuhan Ozturk <fbatuhanr@gmail.com>
2023-08-08 18:00:37 +0300

    slider images

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -6,56 +6,69 @@ import {GiHealthPotion} from "react-icons/gi";
 
 const Categories = () => {
     return (
-        <div>
+        <div className="mt-4">
             <Row>
                 <Col>
                     <h4 className="text-center m-0">SHOP BY CATEGORY</h4>
                 </Col>
             </Row>
             <Row>
-                <Navbar className="gy-4">
-                    <Col xs={3}>
-                        <a href="demo3-shop.html">
-                            <figure>
-                                <img src="" alt="category" />
-                            </figure>
-                            <div className="category-content">
-                                <h3>Sunglasses</h3>
-                                <span><mark className="count">8</mark> products</span>
-                            </div>
-                        </a>
-                    </Col>
-                    <Col xs={3}>
-                        <a href="#" className="p-1 d-flex flex-column text-center text-decoration-none">
-                            <Button variant="outline-dark" className="mx-auto p-3 mb-2">
-                                <BiHome />
-                            </Button>
-                            <div className="text-dark">
-                                <span>Home</span>
-                            </div>
-                        </a>
-                    </Col>
-                    <Col xs={3}>
-                        <a href="#" className="p-1 d-flex flex-column text-center text-decoration-none">
-                            <Button variant="outline-dark" className="mx-auto p-3 mb-2">
-                                <GiHealthPotion />
-                            </Button>
-                            <div className="text-dark">
-                                <span>Beauty</span>
-                            </div>
-                        </a>
-                    </Col>
-                    <Col xs={3}>
-                        <a href="#" className="p-1 d-flex flex-column text-center text-decoration-none">
-                            <Button variant="outline-dark" className="mx-auto p-3 mb-2">
-                                <MdSportsMartialArts />
-                            </Button>
-                            <div className="text-dark">
-                                <span>Sports</span>
-                            </div>
-                        </a>
-                    </Col>
-                </Navbar>
+                <Col xs={{span:8, offset:2}}>
+                    <Navbar className="row gy-4">
+                        <Col xs={3}>
+                            <a href="#" className="text-decoration-none text-center text-dark">
+                                <figure className="mb-2">
+                                    <img src="https://raw.githubusercontent.com/fbatuhanr/ShoppingCart-ReactJS/f95180afe5932e3da9aa1cdfe0c2dc64f9384794/src/assets/categories/electronics.jpeg" alt="category"
+                                         className="d-block w-100 rounded"
+                                    />
+                                </figure>
+                                <div className="p-1">
+                                    <h5 className="mb-1 text-uppercase">Electronics</h5>
+                                    <span className="fs-6 fw-light">8 products</span>
+                                </div>
+                            </a>
+                        </Col>
+                        <Col xs={3}>
+                            <a href="#" className="text-decoration-none text-center text-dark">
+                                <figure className="mb-2">
+                                    <img src="https://raw.githubusercontent.com/fbatuhanr/ShoppingCart-ReactJS/f95180afe5932e3da9aa1cdfe0c2dc64f9384794/src/assets/categories/fashion.jpeg" alt="category"
+                                         className="d-block w-100 rounded"
+                                    />
+                                </figure>
+                                <div className="p-1">
+                                    <h5 className="mb-1 text-uppercase">Fashion</h5>
+                                    <span className="fs-6 fw-light">8 products</span>
+                                </div>
+                            </a>
+                        </Col>
+                        <Col xs={3}>
+                            <a href="#" className="text-decoration-none text-center text-dark">
+                                <figure className="mb-2">
+                                    <img src="https://raw.githubusercontent.com/fbatuhanr/ShoppingCart-ReactJS/f95180afe5932e3da9aa1cdfe0c2dc64f9384794/src/assets/categories/bags.jpeg" alt="category"
+                                         className="d-block w-100 rounded"
+                                    />
+                                </figure>
+                                <div className="p-1">
+                                    <h5 className="mb-1 text-uppercase">Bags</h5>
+                                    <span className="fs-6 fw-light">8 products</span>
+                                </div>
+                            </a>
+                        </Col>
+                        <Col xs={3}>
+                            <a href="#" className="text-decoration-none text-center text-dark">
+                                <figure className="mb-2">
+                                    <img src="https://raw.githubusercontent.com/fbatuhanr/ShoppingCart-ReactJS/f95180afe5932e3da9aa1cdfe0c2dc64f9384794/src/assets/categories/sunglasses.jpeg" alt="category"
+                                         className="d-block w-100 rounded"
+                                    />
+                                </figure>
+                                <div className="p-1">
+                                    <h5 className="mb-1 text-uppercase">Sunglasses</h5>
+                                    <span className="fs-6 fw-light">8 products</span>
+                                </div>
+                            </a>
+                        </Col>
+                    </Navbar>
+                </Col>
             </Row>
         </div>
     );
